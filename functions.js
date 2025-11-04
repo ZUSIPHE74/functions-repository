93dfd7a79de6aa5e0c291d82747b74f983a1d7c1
ZUSIPHE74 <sidimbazusiphe@gmail.com>
2025-11-04 14:43:08 +0200

    initial commit

---
+++ functions.js
@@ -0,0 +1,16 @@
+// TODO: Create a function called greet that takes a name parameter 
+// TODO: Make the greet function return "Hello, [name]!" 
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+// TODO: Create a function called add that takes two parameters 
+// TODO: Make the add function return the sum of both parameters 
+function add(a, b) {
+    return a + b;
+}
+// TODO: Call both functions and store their results in variables 
+const greetingMessage = greet("Alice");
+const additionResult = add(5, 10);
+// TODO: Print the results to the console
+console.log(greetingMessage);
+console.log("Addition Result: " + additionResult);
